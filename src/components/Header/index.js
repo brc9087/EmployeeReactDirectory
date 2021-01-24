d34cc8f5d2247ecb0fabec00c8c4972540d2f65d
Brian Cardines <bcardines811@gmail.com>
2021-01-23 23:57:43 -0500

    headerAndCleanup

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+const style = {
+    headerStyle: {
+        color: "white",
+        background: "blue",
+        width: "100%",
+        textAlign: "center",
+        borderBottom: "solid orange",
+        marginBottom: "10px",
+        fontSize: "100px"
+    }
+}
+
+function Header() {
+
+    return (
+        <div style={style.headerStyle} className="header">
+            <h1>Employee Directory</h1>
+            </div>
+    )
+}
+
+export default Header;
